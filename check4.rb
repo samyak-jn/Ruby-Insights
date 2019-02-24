423499af5610e3d6ee5c7ef85f8b983f541c540b
Samyak Jain <samyak.jn11@gmail.com>
2019-02-24 22:59:18 +0530

    Testing random values with loops.

---
+++ check4.rb
@@ -0,0 +1,28 @@
+# Pick a random number.
+rno = rand(100) + 1
+print "Your magic number is ", rno, "\n"
+
+# Perform all sort of totally uselss test on it and report the results.
+if rno % 2 == 1 then
+    print "An odd number.\n"
+    else
+        print "An even number.\n"
+            if rno > 2 then
+                    print "Not prime"
+                        end
+                        end
+
+                        if rno > 50 
+                            puts " more than half as big"
+            elsif rno == 42
+             print "That's the ultimate magic number!!!!\n"
+           elsif rno < 10
+           print "That's pretty small, actually."
+                                        else
+                    print "What a boring number.\n"
+                    end
+                    if rno == 100
+                     then
+                     print "maxxed out!\n" 
+                    end
+                                        
