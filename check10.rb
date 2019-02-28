aca48f55e05f5ad27285cd5b6a2b4143ad197aa9
Samyak Jain <samyak.jn11@gmail.com>
2019-02-28 23:21:06 +0530

    analyzer+simple calc.

--- check10.rb
+++ check10.rb
@@ -7,3 +7,20 @@ name = first +last
 puts "Your full name is #{first} #{last}"
 puts "Your full name reversed is #{last.reverse} #{first.reverse}"
 puts "Your name has #{name.length} characters in it"
+
+
+#Simple calculator
+puts "Simple calculator"
+25.times {print "-"}
+puts "\nEnter your first number"
+    first_no=gets.chomp
+    first_no=first_no.to_f
+puts "Enter your second number"
+    last_no=gets.chomp
+    last_no=last_no.to_f
+        puts "Basic calc are as follows :"
+        puts "#{first_no} * #{last_no} = #{first_no * last_no}"
+        puts "#{first_no} + #{last_no} = #{first_no + last_no}"
+        puts "#{first_no} - #{last_no} = #{first_no - last_no}"
+        puts "#{first_no} / #{last_no} = #{first_no / last_no}"
+        puts "#{first_no} % #{last_no} = #{first_no % last_no}"
