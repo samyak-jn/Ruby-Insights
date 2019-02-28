5638a0ad87104367b23e851d549ab4a7d01a9453
Samyak Jain <samyak.jn11@gmail.com>
2019-02-28 22:30:43 +0530

    Analyzer example.

---
+++ check10.rb
@@ -0,0 +1,9 @@
+#analyzer
+puts "Enter your first name"
+    first=gets.chomp
+puts "Enter your last name"
+    last=gets.chomp
+name = first +last
+puts "Your full name is #{first} #{last}"
+puts "Your full name reversed is #{last.reverse} #{first.reverse}"
+puts "Your name has #{name.length} characters in it"
