f5a8a5bc66b814b8e16df356021a638fbf9ca2ea
Samyak Jain <samyak.jn11@gmail.com>
2019-03-07 21:26:09 +0530

    Gems introduction.

---
+++ check16.rb
@@ -0,0 +1,15 @@
+#salt  changes some different ending using different hasing
+#adding of another gems, add gems  which require bundler
+#how to add a gem in repo
+=begin
+in local machine
+in terminal
+     gem install bcrypt
+=end
+         #in any repo    
+         require 'bundler/inline'
+         gemfile true do
+         source 'http://rubygems.org'
+         gem 'bcrypt'
+         end
+                         
