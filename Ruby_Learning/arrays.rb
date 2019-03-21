499e5df1123eda081c8c7dae76a3d0d804d37382
Samyak Jain <samyak.jn11@gmail.com>
2019-03-21 22:32:17 +0530

    Updation

--- Ruby_Learning/arrays.rb
+++ Ruby_Learning/arrays.rb
@@ -13,6 +13,22 @@ p arr_words[2..4] # => ["great", "day", "today!"]
 puts arr_words.join(',') # => what,a,great,day,today!
 
 
+a = [1, 3, 4, 7, 8, 10] 
+a.each { |num| print num } # => 1347810 (no new line) 
+puts # => (print new line) 
+
+new_arr = a.select { |num| num > 4 } 
+p new_arr # => [7, 8, 10] 
+new_arr = a.select { |num| num < 10 }
+           .reject{ |num| num.even? } 
+p new_arr # => [1, 3, 7] 
+
+# Multiply each element by 3 producing new array
+new_arr = a.map {|x| x * 3} 
+p new_arr # => [3, 9, 12, 21, 24, 30] 
+
+
+
 # You want a stack (LIFO)? Sure 
 stack = []; stack << "one"; stack.push ("two") 
 puts stack.pop # => two 
@@ -28,4 +44,3 @@ p a.sample(2) # => 2 random elements
 a[6] = 33 
 p a # => [5, 4, 3, 2, nil, nil, 33]
 
-	
