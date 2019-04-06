37f8c4d18d4899b17327b4fef423bafdbbbeea76
Samyak Jain <samyak.jn11@gmail.com>
2019-04-06 22:42:15 +0530

    Access Control

---
+++ Ruby_Learning/encapsulation.rb
@@ -0,0 +1,11 @@
+  def initialize(speed, comfort)
+    @rating = speed * comfort
+  end
+   
+  # Can't SET rating from outside
+  def rating
+    @rating
+  end
+end
+
+puts Car.new(4, 5).rating # => 20
